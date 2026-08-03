4f63ca2b7fcb368a688b93d1c3340d3269a5a47f
byFranca <miguel.sou.franca@gmail.com>
2026-08-03 01:08:16 -0300

    ação dos botoes

--- src/TelaTeatro.java
+++ src/TelaTeatro.java
@@ -7,20 +7,25 @@ public class TelaTeatro {
     int colunas = 10;
     char[][] assentos = new char[linhas][colunas];
     JButton[][] botoes = new JButton[linhas][colunas];
+
+
     double precoInteiro;
     double precoReserva;
     double porcentagemDaReserva = 0.40;
     int modo = 0;
     int quantidade = 0;
-    Color verde = new Color(17, 188, 22);
-    Color amarelo =new Color(217, 217, 22);
-    Color vermelho = new Color(248, 9, 0);
 
-    JLabel lblModo;
+
+    Color verde = new Color(17, 188, 22);
+    Color amarelo = new Color(217, 217, 22);
+    Color vermelho = new Color(248, 9, 0);
+    Color azul = new Color(11, 65, 175, 255);
+
+
     JFrame frame = new JFrame();
 
     public TelaTeatro() {
-        pedirPreco();
+        //pedirPreco();
 
         for (int i = 0; i < linhas; i++) {
             for (int j = 0; j < colunas; j++) {
@@ -50,6 +55,24 @@ public class TelaTeatro {
         }
         JScrollPane gradeScroll = new JScrollPane(grade);
         frame.add(gradeScroll, BorderLayout.CENTER);
+
+        //painel superior
+        JPanel painelSuperior = new JPanel(new BorderLayout());
+        painelSuperior.setBorder(BorderFactory.createEmptyBorder(10, 10, 5, 10));
+
+        JLabel titulo = new JLabel("TEATRO - Sistema de Reservas", SwingConstants.CENTER);
+        titulo.setFont(new Font("Arial", Font.BOLD, 22));
+        painelSuperior.add(titulo, BorderLayout.NORTH);
+
+        JLabel lblModo = new JLabel("Modo atual: Visualização", SwingConstants.CENTER);
+        lblModo.setFont(new Font("Arial", Font.BOLD, 14));
+        painelSuperior.add(lblModo, BorderLayout.CENTER);
+
+        JLabel lblPreco = new JLabel("Preço do ingresso inteiro: " + precoInteiro + "R$", SwingConstants.CENTER);
+        lblPreco.setFont(new Font("Arial", Font.BOLD, 12));
+        painelSuperior.add(lblPreco, BorderLayout.SOUTH);
+
+        frame.add(painelSuperior, BorderLayout.NORTH);
 
 
         //painel lateral
@@ -83,26 +106,68 @@ public class TelaTeatro {
 
         frame.add(painelLateral, BorderLayout.EAST);
 
-        //painel superior
-        JPanel painelSuperior = new JPanel(new BorderLayout());
-        painelSuperior.setBorder(BorderFactory.createEmptyBorder(10, 10, 5, 10));
+        //ação dos botoes
+        btnSair.addActionListener(e -> {
+            int opc = JOptionPane.showConfirmDialog(null, "Deseja mesmo sair do sistema?", "Sair?", JOptionPane.YES_NO_OPTION);
+            if (opc == JOptionPane.YES_OPTION) {
+                System.exit(0);
+            }
+        });
 
-        JLabel titulo = new JLabel("TEATRO - Sistema de Reservas", SwingConstants.CENTER);
-        titulo.setFont(new Font("Arial", Font.BOLD, 22));
-        painelSuperior.add(titulo, BorderLayout.NORTH);
+        btnReserva.addActionListener(e -> {
+            quantidade = pedirQuantidade("reservar", "reserva");
+            if (quantidade != -1) {
+                modo = 1;
+                lblModo.setText("Modo atual: Reserva  -  clique no(s) " + quantidade + " assentos livres que deseja reservar!");
+                lblModo.setForeground(azul);
 
-        JLabel lblModo = new JLabel("Modo atual: n faço a menor ideia", SwingConstants.CENTER);
-        lblModo.setFont(new Font("Arial", Font.BOLD, 14));
-        painelSuperior.add(lblModo, BorderLayout.CENTER);
+            }
+        });
 
-        JLabel lblPreco = new JLabel("Preço do ingresso inteiro: "+precoInteiro+"R$", SwingConstants.CENTER);
-        lblPreco.setFont(new Font("Arial", Font.BOLD, 12));
-        painelSuperior.add(lblPreco, BorderLayout.SOUTH);
+        btnCompra.addActionListener(e ->{
+            quantidade = pedirQuantidade("Comprar", "compra");
+            if(quantidade != -1){
+                modo = 2;
+                lblModo.setText("Modo atual: compra  -  clique no(s) " + quantidade + " assentos livres que deseja comprar!");
+                lblModo.setForeground(vermelho);
+            }
+        });
 
-        frame.add(painelSuperior, BorderLayout.NORTH);
+        btnCancelaReserva.addActionListener(e ->{
+            quantidade = pedirQuantidade("cancelar reserva", "cancelar");
+            if(quantidade != -1){
+                modo = 3;
+                lblModo.setText("Modo atual: Cancelamento de reserva  -  clique no(s) " + quantidade + " reservados que deseja cancelar!");
+                lblModo.setForeground(amarelo);
+            }
+        });
 
 
         frame.setVisible(true);
+    }
+
+    public int pedirQuantidade(String acao, String titulo) {
+        String entrada = JOptionPane.showInputDialog(null, "Quantos assentos deseja " + acao + "?", titulo, JOptionPane.QUESTION_MESSAGE);
+
+        if (entrada == null) {
+            return -1;
+        }
+
+        try {
+            int qtd = Integer.parseInt(entrada);
+
+            if (qtd <= 0) {
+                JOptionPane.showMessageDialog(null, "A quantidade deve ser maior que zero!!");
+                return -1;
+            }
+
+            return qtd;
+
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(null, "Insira um numero válido");
+            return -1;
+        }
+
     }
 
     public void pedirPreco() {
